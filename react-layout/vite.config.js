e79950d78517b285d150f052ddf497c00b356e8e
Zahra Rostami <zahrarostami137878@gmail.com>
2025-05-09 10:38:41 +0330

    feat: add tailwind

--- react-layout/vite.config.js
+++ react-layout/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+  ],
+  server:{
+    host: "127.0.0.1",
+    port: 40000
+  }
 })
