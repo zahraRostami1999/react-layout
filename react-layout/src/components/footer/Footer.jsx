e641ee79b9fe35dd5387a20c348888d97ac49052
Zahra Rostami <zahrarostami137878@gmail.com>
2025-05-10 19:55:01 +0330

    feat: add Footer component with social media links and copyright section

---
+++ react-layout/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div >
+      <div className='h-12 bg-orange-200'>Social media</div>
+      <div className='h-30 bg-orange-300'>Link</div>
+      <div className='h-6 bg-orange-500'>Copy</div>
+    </div>
+  )
+}
+
+export default Footer;
