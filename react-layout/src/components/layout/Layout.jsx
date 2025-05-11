ee798efe9ec94fba608f0948295367f6347d987d
Zahra Rostami <zahrarostami137878@gmail.com>
2025-05-11 22:52:19 +0330

    feat: enhance Layout component styles for responsive design

--- react-layout/src/components/layout/Layout.jsx
+++ react-layout/src/components/layout/Layout.jsx
@@ -5,14 +5,13 @@ import Footer from '../footer/Footer';
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="fixed top-0 w-full bg-orange-300 z-50 py-6 px-10">
+      <header className="fixed top-0 w-full bg-orange-300 z-50 lg:py-6 md:py-4 py-2 sm:py-2 lg:px-10 md:px-8 sm:px-5 px-3 lg:text-lg sm:text-sm text-xs">
         <Header />
       </header>
-
-      <main className="flex-1 py-[120px] bg-red-100 px-10">
+      <main className="flex-1 lg:py-[90px] md:py-[70px] py-[50px] bg-red-100 lg:px-10 md:px-8 sm:px-5 px-3 lg:text-lg sm:text-sm text-xs">
         {children}
       </main>
-      <footer className="w-full  bg-green-300 py-3 px-10">
+      <footer className="w-full  bg-green-300 lg:py-3 py-2 lg:px-10 md:px-8 sm:px-5 px-3 lg:text-lg sm:text-sm text-xs">
         <Footer />
       </footer>
     </div>
